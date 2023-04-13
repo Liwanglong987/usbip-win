38bf6cec9a32ba013f5c21e28eb1b68ae56fae3a
Wanglong Li <Wanglong.Li@ontoinnovation.com>
2023-04-13 17:33:41 +0800

    换一下函数位置以供使用

--- userspace/lib/usbip_forward.c
+++ userspace/lib/usbip_forward.c
@@ -5,34 +5,11 @@
 
 #include "usbip_proto.h"
 #include "usbip_network.h"
+#include "usbip_forward.h"
 
-#define BUFREAD_P(devbuf)	((devbuf)->offp - (devbuf)->offhdr)
-#define BUFREADMAX_P(devbuf)	((devbuf)->bufmaxp - (devbuf)->offp)
-#define BUFREMAIN_C(devbuf)	((devbuf)->bufmaxc - (devbuf)->offc)
-#define BUFHDR_P(devbuf)	((devbuf)->bufp + (devbuf)->offhdr)
-#define BUFCUR_P(devbuf)	((devbuf)->bufp + (devbuf)->offp)
-#define BUFCUR_C(devbuf)	((devbuf)->bufc + (devbuf)->offc)
 
-typedef struct _devbuf {
-	const char	*desc;
-	BOOL	is_req, swap_req;
-	BOOL	invalid;
-	/* asynchronous read is in progress */
-	BOOL	in_reading;
-	/* asynchronous write is in progress */
-	BOOL	in_writing;
-	/* step 1: reading header, 2: reading data */
-	int	step_reading;
-	HANDLE	hdev;
-	char	*bufp, *bufc;	/* bufp: producer, bufc: consumer */
-	DWORD	offhdr;		/* header offset for producer */
-	DWORD	offp, offc;	/* offp: producer offset, offc: consumer offset */
-	DWORD	bufmaxp, bufmaxc;
-	struct _devbuf	*peer;
-	OVERLAPPED	ovs[2];
-	/* completion event for read or write */
-	HANDLE	hEvent;
-} devbuf_t;
+
+
 
 /*
  * Two devbuf's are shared via hEvent, which indicates read or write completion.
@@ -45,15 +22,15 @@ static HANDLE	hEvent;
 #undef USING_STDOUT
 
 static void
-dbg_to_file(char *fmt, ...)
+dbg_to_file(char* fmt, ...)
 {
-	FILE	*fp;
+	FILE* fp;
 	va_list ap;
 
 #ifdef USING_STDOUT
 	fp = stdout;
 #else
-	if (fopen_s(&fp, "debug_pdu.log", "a+") != 0)
+	if(fopen_s(&fp, "debug_pdu.log", "a+") != 0)
 		return;
 #endif
 	va_start(ap, fmt);
@@ -64,10 +41,10 @@ dbg_to_file(char *fmt, ...)
 #endif
 }
 
-static const char *
+static const char*
 dbg_usbip_hdr_cmd(unsigned int cmd)
 {
-	switch (cmd) {
+	switch(cmd) {
 	case USBIP_CMD_SUBMIT:
 		return "CMD_SUBMIT";
 	case USBIP_RET_SUBMIT:
@@ -82,44 +59,44 @@ dbg_usbip_hdr_cmd(unsigned int cmd)
 }
 
 static void
-dump_iso_pkts(struct usbip_header *hdr)
+dump_iso_pkts(struct usbip_header* hdr)
 {
-	struct usbip_iso_packet_descriptor	*iso_desc;
+	struct usbip_iso_packet_descriptor* iso_desc;
 	int	n_pkts;
 	int	i;
 
-	switch (hdr->base.command) {
+	switch(hdr->base.command) {
 	case USBIP_CMD_SUBMIT:
 		n_pkts = hdr->u.cmd_submit.number_of_packets;
-		if (hdr->base.direction)
-			iso_desc = (struct usbip_iso_packet_descriptor *)(hdr + 1);
+		if(hdr->base.direction)
+			iso_desc = (struct usbip_iso_packet_descriptor*)(hdr + 1);
 		else
-			iso_desc = (struct usbip_iso_packet_descriptor *)((char *)(hdr + 1) + hdr->u.cmd_submit.transfer_buffer_length);
+			iso_desc = (struct usbip_iso_packet_descriptor*)((char*)(hdr + 1) + hdr->u.cmd_submit.transfer_buffer_length);
 		break;
 	case USBIP_RET_SUBMIT:
 		n_pkts = hdr->u.ret_submit.number_of_packets;
-		if (hdr->base.direction)
-			iso_desc = (struct usbip_iso_packet_descriptor *)((char *)(hdr + 1) + hdr->u.ret_submit.actual_length);
+		if(hdr->base.direction)
+			iso_desc = (struct usbip_iso_packet_descriptor*)((char*)(hdr + 1) + hdr->u.ret_submit.actual_length);
 		else
-			iso_desc = (struct usbip_iso_packet_descriptor *)(hdr + 1);
+			iso_desc = (struct usbip_iso_packet_descriptor*)(hdr + 1);
 		break;
 	default:
 		return;
 	}
 
-	for (i = 0; i < n_pkts; i++) {
+	for(i = 0; i < n_pkts; i++) {
 		dbg_to_file("  o:%d,l:%d,al:%d,st:%d\n", iso_desc->offset, iso_desc->length, iso_desc->actual_length, iso_desc->status);
 		iso_desc++;
 	}
 }
 
 static void
-dump_usbip_header(struct usbip_header *hdr)
+dump_usbip_header(struct usbip_header* hdr)
 {
 	dbg_to_file("DUMP: %s,seq:%u,devid:%x,dir:%s,ep:%x\n",
-		dbg_usbip_hdr_cmd(hdr->base.command), hdr->base.seqnum, hdr->base.devid, hdr->base.direction ? "in": "out", hdr->base.ep);
+		dbg_usbip_hdr_cmd(hdr->base.command), hdr->base.seqnum, hdr->base.devid, hdr->base.direction ? "in" : "out", hdr->base.ep);
 
-	switch (hdr->base.command) {
+	switch(hdr->base.command) {
 	case USBIP_CMD_SUBMIT:
 		dbg_to_file("  flags:%x,len:%x,sf:%x,#p:%x,intv:%x\n",
 			hdr->u.cmd_submit.transfer_flags,
@@ -166,19 +143,19 @@ dump_usbip_header(struct usbip_header *hdr)
 #endif
 
 static void
-swap_usbip_header_base_endian(struct usbip_header_basic *base)
+swap_usbip_header_base_endian(struct usbip_header_basic* base)
 {
-	base->command	= htonl(base->command);
-	base->seqnum	= htonl(base->seqnum);
-	base->devid	= htonl(base->devid);
-	base->direction	= htonl(base->direction);
-	base->ep	= htonl(base->ep);
+	base->command = htonl(base->command);
+	base->seqnum = htonl(base->seqnum);
+	base->devid = htonl(base->devid);
+	base->direction = htonl(base->direction);
+	base->ep = htonl(base->ep);
 }
 
 static void
-swap_cmd_submit_endian(struct usbip_header_cmd_submit *pdu)
+swap_cmd_submit_endian(struct usbip_header_cmd_submit* pdu)
 {
-	pdu->transfer_flags	= ntohl(pdu->transfer_flags);
+	pdu->transfer_flags = ntohl(pdu->transfer_flags);
 	pdu->transfer_buffer_length = ntohl(pdu->transfer_buffer_length);
 	pdu->start_frame = ntohl(pdu->start_frame);
 	pdu->number_of_packets = ntohl(pdu->number_of_packets);
@@ -186,7 +163,7 @@ swap_cmd_submit_endian(struct usbip_header_cmd_submit *pdu)
 }
 
 static void
-swap_ret_submit_endian(struct usbip_header_ret_submit *pdu)
+swap_ret_submit_endian(struct usbip_header_ret_submit* pdu)
 {
 	pdu->status = ntohl(pdu->status);
 	pdu->actual_length = ntohl(pdu->actual_length);
@@ -196,21 +173,21 @@ swap_ret_submit_endian(struct usbip_header_ret_submit *pdu)
 }
 
 static void
-swap_cmd_unlink_endian(struct usbip_header_cmd_unlink *pdu)
+swap_cmd_unlink_endian(struct usbip_header_cmd_unlink* pdu)
 {
 	pdu->seqnum = ntohl(pdu->seqnum);
 }
 
 static void
-swap_ret_unlink_endian(struct usbip_header_ret_unlink *pdu)
+swap_ret_unlink_endian(struct usbip_header_ret_unlink* pdu)
 {
 	pdu->status = ntohl(pdu->status);
 }
 
 static void
-swap_usbip_header_cmd(unsigned int cmd, struct usbip_header *hdr)
+swap_usbip_header_cmd(unsigned int cmd, struct usbip_header* hdr)
 {
-	switch (cmd) {
+	switch(cmd) {
 	case USBIP_CMD_SUBMIT:
 		swap_cmd_submit_endian(&hdr->u.cmd_submit);
 		break;
@@ -231,11 +208,11 @@ swap_usbip_header_cmd(unsigned int cmd, struct usbip_header *hdr)
 }
 
 static void
-swap_usbip_header_endian(struct usbip_header *hdr, BOOL from_swapped)
+swap_usbip_header_endian(struct usbip_header* hdr, BOOL from_swapped)
 {
 	unsigned int	cmd;
 
-	if (from_swapped) {
+	if(from_swapped) {
 		swap_usbip_header_base_endian(&hdr->base);
 		cmd = hdr->base.command;
 	}
@@ -247,13 +224,13 @@ swap_usbip_header_endian(struct usbip_header *hdr, BOOL from_swapped)
 }
 
 static void
-swap_iso_descs_endian(char *buf, int num)
+swap_iso_descs_endian(char* buf, int num)
 {
-	struct usbip_iso_packet_descriptor	*ip_desc;
+	struct usbip_iso_packet_descriptor* ip_desc;
 	int i;
 
-	ip_desc = (struct usbip_iso_packet_descriptor *)buf;
-	for (i = 0; i < num; i++) {
+	ip_desc = (struct usbip_iso_packet_descriptor*)buf;
+	for(i = 0; i < num; i++) {
 		ip_desc->offset = ntohl(ip_desc->offset);
 		ip_desc->status = ntohl(ip_desc->status);
 		ip_desc->length = ntohl(ip_desc->length);
@@ -291,7 +268,7 @@ struct usbip_cached_hdr {
 static struct usbip_cached_hdr hdrs_cache[HDRS_CACHE_SIZE];
 
 static inline void
-hdrs_cache_insert(struct usbip_header *usbip_hdr)
+hdrs_cache_insert(struct usbip_header* usbip_hdr)
 {
 	int	idx = usbip_hdr->base.seqnum % HDRS_CACHE_SIZE;
 
@@ -300,11 +277,11 @@ hdrs_cache_insert(struct usbip_header *usbip_hdr)
 }
 
 static inline UINT32
-hdrs_cache_direction(struct usbip_header *usbip_hdr)
+hdrs_cache_direction(struct usbip_header* usbip_hdr)
 {
 	int	idx = usbip_hdr->base.seqnum % HDRS_CACHE_SIZE;
 
-	if (usbip_hdr->base.seqnum == hdrs_cache[idx].seqnum) {
+	if(usbip_hdr->base.seqnum == hdrs_cache[idx].seqnum) {
 		/* Restore packet direction! */
 		usbip_hdr->base.direction = hdrs_cache[idx].direction;
 		return hdrs_cache[idx].direction;
@@ -316,57 +293,57 @@ hdrs_cache_direction(struct usbip_header *usbip_hdr)
 }
 
 static int
-get_xfer_len(BOOL is_req, struct usbip_header *hdr)
+get_xfer_len(BOOL is_req, struct usbip_header* hdr)
 {
-	if (is_req) {
-		if (hdr->base.command == USBIP_CMD_UNLINK)
+	if(is_req) {
+		if(hdr->base.command == USBIP_CMD_UNLINK)
 			return 0;
 		hdrs_cache_insert(hdr);
-		if (hdr->base.direction)
+		if(hdr->base.direction)
 			return 0;
 		return hdr->u.cmd_submit.transfer_buffer_length;
 	}
 	else {
-		if (hdr->base.command == USBIP_RET_UNLINK)
+		if(hdr->base.command == USBIP_RET_UNLINK)
 			return 0;
-		if (hdrs_cache_direction(hdr) == USBIP_DIR_OUT)
+		if(hdrs_cache_direction(hdr) == USBIP_DIR_OUT)
 			return 0;
 		return hdr->u.ret_submit.actual_length;
 	}
 }
 
 static int
-get_iso_len(BOOL is_req, struct usbip_header *hdr)
+get_iso_len(BOOL is_req, struct usbip_header* hdr)
 {
-	if (is_req) {
-		if (hdr->base.command == USBIP_CMD_UNLINK)
+	if(is_req) {
+		if(hdr->base.command == USBIP_CMD_UNLINK)
 			return 0;
 		return hdr->u.cmd_submit.number_of_packets * sizeof(struct usbip_iso_packet_descriptor);
 	}
 	else {
-		if (hdr->base.command == USBIP_RET_UNLINK)
+		if(hdr->base.command == USBIP_RET_UNLINK)
 			return 0;
 		return hdr->u.ret_submit.number_of_packets * sizeof(struct usbip_iso_packet_descriptor);
 	}
 }
 
 static BOOL
-setup_rw_overlapped(devbuf_t *buff)
+setup_rw_overlapped(devbuf_t* buff)
 {
 	int	i;
 
-	for (i = 0; i < 2; i++) {
+	for(i = 0; i < 2; i++) {
 		memset(&buff->ovs[i], 0, sizeof(OVERLAPPED));
 		buff->ovs[i].hEvent = (HANDLE)buff;
 	}
 	return TRUE;
 }
 
-static BOOL
-init_devbuf(devbuf_t *buff, const char *desc, BOOL is_req, BOOL swap_req, HANDLE hdev, HANDLE hEvent)
+BOOL
+init_devbuf(devbuf_t* buff, const char* desc, BOOL is_req, BOOL swap_req, HANDLE hdev, HANDLE hEvent)
 {
-	buff->bufp = (char *)malloc(1024);
-	if (buff->bufp == NULL)
+	buff->bufp = (char*)malloc(1024);
+	if(buff->bufp == NULL)
 		return FALSE;
 	buff->bufc = buff->bufp;
 	buff->desc = desc;
@@ -383,7 +360,7 @@ init_devbuf(devbuf_t *buff, const char *desc, BOOL is_req, BOOL swap_req, HANDLE
 	buff->bufmaxc = 0;
 	buff->hdev = hdev;
 	buff->hEvent = hEvent;
-	if (!setup_rw_overlapped(buff)) {
+	if(!setup_rw_overlapped(buff)) {
 		free(buff->bufp);
 		return FALSE;
 	}
@@ -391,25 +368,25 @@ init_devbuf(devbuf_t *buff, const char *desc, BOOL is_req, BOOL swap_req, HANDLE
 }
 
 static void
-cleanup_devbuf(devbuf_t *buff)
+cleanup_devbuf(devbuf_t* buff)
 {
 	free(buff->bufp);
-	if (buff->bufp != buff->bufc)
+	if(buff->bufp != buff->bufc)
 		free(buff->bufc);
 }
 
 static VOID CALLBACK
 read_completion(DWORD errcode, DWORD nread, LPOVERLAPPED lpOverlapped)
 {
-	devbuf_t	*rbuff;
+	devbuf_t* rbuff;
 
-	rbuff = (devbuf_t *)lpOverlapped->hEvent;
-	if (errcode == 0) {
+	rbuff = (devbuf_t*)lpOverlapped->hEvent;
+	if(errcode == 0) {
 		rbuff->offp += nread;
-		if (nread == 0)
+		if(nread == 0)
 			rbuff->invalid = TRUE;
 	}
-	else if (errcode == ERROR_DEVICE_NOT_CONNECTED) {
+	else if(errcode == ERROR_DEVICE_NOT_CONNECTED) {
 		rbuff->invalid = TRUE;
 	}
 	rbuff->in_reading = FALSE;
@@ -417,17 +394,17 @@ read_completion(DWORD errcode, DWORD nread, LPOVERLAPPED lpOverlapped)
 }
 
 static BOOL
-read_devbuf(devbuf_t *rbuff, DWORD nreq)
+read_devbuf(devbuf_t* rbuff, DWORD nreq)
 {
-	if (BUFREADMAX_P(rbuff) < nreq) {
-		char	*bufnew;
+	if(BUFREADMAX_P(rbuff) < nreq) {
+		char* bufnew;
 
-		if (rbuff->bufp != rbuff->bufc) {
+		if(rbuff->bufp != rbuff->bufc) {
 			/* reallocation is allowed only if producer and consumer use their own buffers */
 			DWORD	nmore = nreq - BUFREADMAX_P(rbuff);
 
-			bufnew = (char *)realloc(rbuff->bufp, rbuff->bufmaxp + nmore);
-			if (bufnew == NULL) {
+			bufnew = (char*)realloc(rbuff->bufp, rbuff->bufmaxp + nmore);
+			if(bufnew == NULL) {
 				dbg("failed to reallocate buffer: %s", rbuff->desc);
 				return FALSE;
 			}
@@ -437,12 +414,12 @@ read_devbuf(devbuf_t *rbuff, DWORD nreq)
 		else {
 			DWORD	nexist = BUFREAD_P(rbuff);
 
-			bufnew = (char *)malloc(nreq + nexist);
-			if (bufnew == NULL) {
+			bufnew = (char*)malloc(nreq + nexist);
+			if(bufnew == NULL) {
 				dbg("failed to allocate buffer: %s", rbuff->desc);
 				return FALSE;
 			}
-			if (nexist > 0) {
+			if(nexist > 0) {
 				/* copy from already read usbip header */
 				memcpy(bufnew, BUFHDR_P(rbuff), nexist);
 			}
@@ -453,11 +430,11 @@ read_devbuf(devbuf_t *rbuff, DWORD nreq)
 		}
 	}
 
-	if (!rbuff->in_reading) {
-		if (!ReadFileEx(rbuff->hdev, BUFCUR_P(rbuff), nreq, &rbuff->ovs[0], read_completion)) {
+	if(!rbuff->in_reading) {
+		if(!ReadFileEx(rbuff->hdev, BUFCUR_P(rbuff), nreq, &rbuff->ovs[0], read_completion)) {
 			DWORD error = GetLastError();
 			dbg("failed to read: err: 0x%lx", error);
-			if (error == ERROR_NETNAME_DELETED) {
+			if(error == ERROR_NETNAME_DELETED) {
 				dbg("could the client have dropped the connection?");
 			}
 			return FALSE;
@@ -470,17 +447,17 @@ read_devbuf(devbuf_t *rbuff, DWORD nreq)
 static VOID CALLBACK
 write_completion(DWORD errcode, DWORD nwrite, LPOVERLAPPED lpOverlapped)
 {
-	devbuf_t	*wbuff, *rbuff;
+	devbuf_t* wbuff, * rbuff;
 
 	wbuff = (devbuf_t*)lpOverlapped->hEvent;
 	wbuff->in_writing = FALSE;
 
 	SetEvent(wbuff->hEvent);
 
-	if (errcode != 0)
+	if(errcode != 0)
 		return;
 
-	if (nwrite == 0) {
+	if(nwrite == 0) {
 		wbuff->invalid = TRUE;
 		return;
 	}
@@ -489,16 +466,16 @@ write_completion(DWORD errcode, DWORD nwrite, LPOVERLAPPED lpOverlapped)
 }
 
 static BOOL
-write_devbuf(devbuf_t *wbuff, devbuf_t *rbuff)
+write_devbuf(devbuf_t* wbuff, devbuf_t* rbuff)
 {
-	if (rbuff->bufp != rbuff->bufc && BUFREMAIN_C(rbuff) == 0) {
+	if(rbuff->bufp != rbuff->bufc && BUFREMAIN_C(rbuff) == 0) {
 		free(rbuff->bufc);
 		rbuff->bufc = rbuff->bufp;
 		rbuff->offc = 0;
 		rbuff->bufmaxc = rbuff->offhdr;
 	}
-	if (!wbuff->in_writing && BUFREMAIN_C(rbuff) > 0) {
-		if (!WriteFileEx(wbuff->hdev, BUFCUR_C(rbuff), BUFREMAIN_C(rbuff), &wbuff->ovs[1], write_completion)) {
+	if(!wbuff->in_writing && BUFREMAIN_C(rbuff) > 0) {
+		if(!WriteFileEx(wbuff->hdev, BUFCUR_C(rbuff), BUFREMAIN_C(rbuff), &wbuff->ovs[1], write_completion)) {
 			dbg("failed to write sock: err: 0x%lx", GetLastError());
 			return FALSE;
 		}
@@ -509,21 +486,21 @@ write_devbuf(devbuf_t *wbuff, devbuf_t *rbuff)
 }
 
 static int
-read_dev(devbuf_t *rbuff, BOOL swap_req_write)
+read_dev(devbuf_t* rbuff, BOOL swap_req_write)
 {
-	struct usbip_header	*hdr;
+	struct usbip_header* hdr;
 	unsigned long	xfer_len, iso_len, len_data;
 
-	if (BUFREAD_P(rbuff) < sizeof(struct usbip_header)) {
+	if(BUFREAD_P(rbuff) < sizeof(struct usbip_header)) {
 		rbuff->step_reading = 1;
-		if (!read_devbuf(rbuff, sizeof(struct usbip_header) - BUFREAD_P(rbuff)))
+		if(!read_devbuf(rbuff, sizeof(struct usbip_header) - BUFREAD_P(rbuff)))
 			return -1;
 		return 0;
 	}
 
-	hdr = (struct usbip_header *)BUFHDR_P(rbuff);
-	if (rbuff->step_reading == 1) {
-		if (rbuff->swap_req)
+	hdr = (struct usbip_header*)BUFHDR_P(rbuff);
+	if(rbuff->step_reading == 1) {
+		if(rbuff->swap_req)
 			swap_usbip_header_endian(hdr, TRUE);
 		rbuff->step_reading = 2;
 	}
@@ -532,42 +509,42 @@ read_dev(devbuf_t *rbuff, BOOL swap_req_write)
 	iso_len = get_iso_len(rbuff->is_req, hdr);
 
 	len_data = xfer_len + iso_len;
-	if (BUFREAD_P(rbuff) < len_data + sizeof(struct usbip_header)) {
+	if(BUFREAD_P(rbuff) < len_data + sizeof(struct usbip_header)) {
 		DWORD	nmore = (DWORD)(len_data + sizeof(struct usbip_header)) - BUFREAD_P(rbuff);
 
-		if (!read_devbuf(rbuff, nmore))
+		if(!read_devbuf(rbuff, nmore))
 			return -1;
 		return 0;
 	}
 
-	if (rbuff->swap_req && iso_len > 0)
-		swap_iso_descs_endian((char *)(hdr + 1) + xfer_len, hdr->u.ret_submit.number_of_packets);
+	if(rbuff->swap_req && iso_len > 0)
+		swap_iso_descs_endian((char*)(hdr + 1) + xfer_len, hdr->u.ret_submit.number_of_packets);
 
 	DBG_USBIP_HEADER(hdr);
 
-	if (swap_req_write) {
-		if (iso_len > 0)
-			swap_iso_descs_endian((char *)(hdr + 1) + xfer_len, hdr->u.ret_submit.number_of_packets);
+	if(swap_req_write) {
+		if(iso_len > 0)
+			swap_iso_descs_endian((char*)(hdr + 1) + xfer_len, hdr->u.ret_submit.number_of_packets);
 		swap_usbip_header_endian(hdr, FALSE);
 	}
 
 	rbuff->offhdr += (sizeof(struct usbip_header) + len_data);
-	if (rbuff->bufp == rbuff->bufc)
+	if(rbuff->bufp == rbuff->bufc)
 		rbuff->bufmaxc = rbuff->offp;
 	rbuff->step_reading = 0;
 
 	return 1;
 }
 
-static BOOL
-read_write_dev(devbuf_t *rbuff, devbuf_t *wbuff)
+BOOL
+read_write_dev(devbuf_t* rbuff, devbuf_t* wbuff)
 {
 	int	res;
 
-	if (!rbuff->in_reading) {
-		if ((res = read_dev(rbuff, wbuff->swap_req)) < 0)
+	if(!rbuff->in_reading) {
+		if((res = read_dev(rbuff, wbuff->swap_req)) < 0)
 			return FALSE;
-		if (res == 0)
+		if(res == 0)
 			return TRUE;
 	}
 	return write_devbuf(wbuff, rbuff);
@@ -590,7 +567,7 @@ usbip_forward(HANDLE hdev_src, HANDLE hdev_dst, BOOL inbound)
 	BOOL	is_req_src;
 	BOOL	swap_req_src, swap_req_dst;
 
-	if (inbound) {
+	if(inbound) {
 		desc_src = "socket";
 		desc_dst = "stub";
 		is_req_src = TRUE;
@@ -606,17 +583,17 @@ usbip_forward(HANDLE hdev_src, HANDLE hdev_dst, BOOL inbound)
 	}
 
 	hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-	if (hEvent == NULL) {
+	if(hEvent == NULL) {
 		dbg("failed to create event");
 		return;
 	}
 
-	if (!init_devbuf(&buff_src, desc_src, TRUE, swap_req_src, hdev_src, hEvent)) {
+	if(!init_devbuf(&buff_src, desc_src, TRUE, swap_req_src, hdev_src, hEvent)) {
 		CloseHandle(hEvent);
 		dbg("failed to initialize %s buffer", desc_src);
 		return;
 	}
-	if (!init_devbuf(&buff_dst, desc_dst, FALSE, swap_req_dst, hdev_dst, hEvent)) {
+	if(!init_devbuf(&buff_dst, desc_dst, FALSE, swap_req_dst, hdev_dst, hEvent)) {
 		CloseHandle(hEvent);
 		dbg("failed to initialize %s buffer", desc_dst);
 		cleanup_devbuf(&buff_src);
@@ -628,15 +605,15 @@ usbip_forward(HANDLE hdev_src, HANDLE hdev_dst, BOOL inbound)
 
 	signal(SIGINT, signalhandler);
 
-	while (!interrupted) {
-		if (!read_write_dev(&buff_src, &buff_dst))
+	while(!interrupted) {
+		if(!read_write_dev(&buff_src, &buff_dst))
 			break;
-		if (!read_write_dev(&buff_dst, &buff_src))
+		if(!read_write_dev(&buff_dst, &buff_src))
 			break;
 
-		if (buff_src.invalid || buff_dst.invalid)
+		if(buff_src.invalid || buff_dst.invalid)
 			break;
-		if (buff_src.in_reading && buff_dst.in_reading &&
+		if(buff_src.in_reading && buff_dst.in_reading &&
 			(buff_src.in_writing || BUFREMAIN_C(&buff_dst) == 0) &&
 			(buff_dst.in_writing || BUFREMAIN_C(&buff_src) == 0)) {
 			WaitForSingleObjectEx(hEvent, INFINITE, TRUE);
@@ -644,17 +621,17 @@ usbip_forward(HANDLE hdev_src, HANDLE hdev_dst, BOOL inbound)
 		}
 	}
 
-	if (interrupted) {
+	if(interrupted) {
 		info("CTRL-C received\n");
 	}
 	signal(SIGINT, SIG_DFL);
 
-	if (buff_src.in_reading)
+	if(buff_src.in_reading)
 		CancelIoEx(hdev_src, &buff_src.ovs[0]);
-	if (buff_dst.in_reading)
+	if(buff_dst.in_reading)
 		CancelIoEx(hdev_dst, &buff_dst.ovs[0]);
 
-	while (buff_src.in_reading || buff_dst.in_reading || buff_src.in_writing || buff_dst.in_writing) {
+	while(buff_src.in_reading || buff_dst.in_reading || buff_src.in_writing || buff_dst.in_writing) {
 		WaitForSingleObjectEx(hEvent, INFINITE, TRUE);
 	}
 
--- userspace/lib/usbip_forward.h
+++ userspace/lib/usbip_forward.h
@@ -1,5 +1,38 @@
 #pragma
 
-#include <winsock2.h>
+#ifndef _USBIP_FORWARD_H_
+#define _USBIP_FORWARD_H_
+
+#define BUFREAD_P(devbuf)	((devbuf)->offp - (devbuf)->offhdr)
+#define BUFREADMAX_P(devbuf)	((devbuf)->bufmaxp - (devbuf)->offp)
+#define BUFREMAIN_C(devbuf)	((devbuf)->bufmaxc - (devbuf)->offc)
+#define BUFHDR_P(devbuf)	((devbuf)->bufp + (devbuf)->offhdr)
+#define BUFCUR_P(devbuf)	((devbuf)->bufp + (devbuf)->offp)
+#define BUFCUR_C(devbuf)	((devbuf)->bufc + (devbuf)->offc)
+
+typedef struct _devbuf {
+	const char* desc;
+	BOOL	is_req, swap_req;
+	BOOL	invalid;
+	/* asynchronous read is in progress */
+	BOOL	in_reading;
+	/* asynchronous write is in progress */
+	BOOL	in_writing;
+	/* step 1: reading header, 2: reading data */
+	int	step_reading;
+	HANDLE	hdev;
+	char* bufp, * bufc;	/* bufp: producer, bufc: consumer */
+	DWORD	offhdr;		/* header offset for producer */
+	DWORD	offp, offc;	/* offp: producer offset, offc: consumer offset */
+	DWORD	bufmaxp, bufmaxc;
+	struct _devbuf* peer;
+	OVERLAPPED	ovs[2];
+	/* completion event for read or write */
+	HANDLE	hEvent;
+} devbuf_t;
 
 void usbip_forward(HANDLE hdev_src, HANDLE hdev_dst, BOOL inbound);
+BOOL init_devbuf(devbuf_t* buff, const char* desc, BOOL is_req, BOOL swap_req, HANDLE hdev, HANDLE hEvent);
+BOOL read_write_dev(devbuf_t* rbuff, devbuf_t* wbuff);
+int read_dev(devbuf_t* rbuff, BOOL swap_req_write);
+#endif
